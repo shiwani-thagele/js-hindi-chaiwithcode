23bd5dfc660339800fc44a477fc1b488d443aeb8
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-10-20 13:24:26 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+// "use strict" -> treate all javascript code as a newer version
+
+//alert(3+3) -> we are using node js not browser so it will run on browser
+
+//console.log(2+3); console.log("shiwani"); // code readability is must avoid this type of code
+
+//documentation's should follow 
+//https://tc39.es/ecma262/ || https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//dataTypes
+
+let name  = "shiwani" //string
+let age = 18;  // number
+let isLoggedIn = false // boolean
+let state;
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value [khali]
+//undefined => state 
+//symbol => unique
+
+//object
+
+console.log(typeof "shiwani"); //typeof define the type of datatype
+console.log(typeof null); // object 
+console.log(typeof undefined); //undefine
+
+
+
+
