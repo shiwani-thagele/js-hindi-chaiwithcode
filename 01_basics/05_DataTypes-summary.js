b032e6122cde9683b96f7f43f6e10a6eeaaa035a
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-10-20 20:44:36 +0000

    datatype summaru

---
+++ 01_basics/05_DataTypes-summary.js
@@ -0,0 +1,13 @@
+//Data Types preferd documentation for better understanding
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+//https://tc39.es/ecma262/#sec-ecmascript-data-types-and-values
+
+// 1.Primitive DataType  - 7 types
+// Primitive : String , Number, Boolean , null, undefined,symbol,BigInt
+
+//2.Reference DataType(Non primitive)
+//Reference : Arrays,Objects,Functions  --> returntype object
+
+//js is dynamic typed language
+
+//****read typeof of all datatype for interview perspective***
