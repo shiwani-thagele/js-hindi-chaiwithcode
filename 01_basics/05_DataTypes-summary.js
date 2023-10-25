4873fa5233c7db58ba169e13a2b8e9eaf5d95176
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-10-25 12:21:54 +0000

    heap and stack memeory

--- 01_basics/05_DataTypes-summary.js
+++ 01_basics/05_DataTypes-summary.js
@@ -11,3 +11,28 @@
 //js is dynamic typed language
 
 //****read typeof of all datatype for interview perspective***
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++ memeory stack and heap memeory ++++++++++++++++
+// Stack (premitive) , Heap(Non - premitive)
+
+let shiwani = "shiwanithagele@gmail.com"; // stack
+
+let shivi = shiwani;
+shivi = "thageleshiwani"
+console.log(shivi);
+console.log(shiwani);
+
+
+let user1 = {
+    email: "shivi@gmail.com",
+    upi: "user01@ybl"
+}
+
+let user2 = user1;
+user2.email = "user@gmail.com"
+console.log(user2.email); //same
+console.log(user1.email); //same
+
+console.log(user1.upi); //same 
+console.log(user2.upi); //same
