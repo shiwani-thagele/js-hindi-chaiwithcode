0f78accba8b849acf2474cc0c006ed03c2fa1f43
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-11-09 13:14:45 +0000

    more about objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,75 @@
+//object singleton /constructor
+
+// const tinderUser = new Object() //singlton
+
+const tinderUser = {} //no diference {non singleton}
+ tinderUser.id ="123abc"
+ tinderUser.name ="shivi"
+ tinderUser.isloggedin =false
+
+
+//console.log(tinderUser);//empty object
+const regularUser = {
+    email:"shivi@gmail.com",
+    fullname :{
+        userfullname:{
+            firstname :"shiwani",
+            lastname : "thagele"
+
+                }
+    }
+}
+//console.log(regularUser.fullname.userfullname.lastname); //nested
+//? => optional chaining  **
+
+const object1 = {
+    1:"a",
+    2:"b"
+}
+
+const object2 ={
+    3:"e",
+    4:"f"
+}
+//const object3 = {object1,object2}
+//console.log(object1,object2);
+//console.log(object3);
+
+
+//assign(target,source)
+//const object3 = Object.assign(object1,object2)
+ //const object3 = Object.assign({},object1,object2) //exact value will get,if we have more arrays then this will gurentee that target merge with source
+// documenation => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign 
+
+
+const object3 = {...object1,...object2} // 90% used{ '1': 'a', '2': 'b', '3': 'e', '4': 'f' }
+//console.log(object3);
+
+
+//values coming from db
+
+const users =[{
+    id:1,
+    email:"shivi@gmail.com"
+},
+{
+    id:2,
+    email:"shivithagel@gmail.com"
+}]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); /**{ id: '123abc', name: 'shivi', isloggedin: false }
+[ 'id', 'name', 'isloggedin' ] */
+console.log(Object.values(tinderUser)) /**{ id: '123abc', name: 'shivi', isloggedin: false }
+[ 'id', 'name', 'isloggedin' ]
+[ '123abc', 'shivi', false ] */
+
+console.log(Object.entries(tinderUser));
+/**{ id: '123abc', name: 'shivi', isloggedin: false }
+[ 'id', 'name', 'isloggedin' ]
+[ '123abc', 'shivi', false ]
+[ [ 'id', '123abc' ], [ 'name', 'shivi' ], [ 'isloggedin', false ] ] */
+
+console.log(tinderUser.hasOwnProperty('isloggedin')); //check for validation
+
