01fb699db7484e5a71f0b11bad16e460274d736b
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-11-17 20:13:56 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,12 @@
+let i =0
+while (i <= 10) {
+    console.log(`value of index is ${i}`); 
+    i = i+2
+}
+
+let myarray  = ["flash" , "batman", "superman"]
+let arr = 0
+while (arr < myarray.length) {
+     console.log(`value is : ${myarray[arr]}`);
+     arr++;
+}
