63ccccd71c63c605c7c2b9aca2d25c4f608ab9fb
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-11-17 18:07:09 +0000

    truthly , nullish coalescing operator , ternary  operator

--- 04_control_flow/three_truthy.js
+++ 04_control_flow/three_truthy.js
@@ -11,9 +11,39 @@ if(userEmail3){
 //false , 0 , -0 , BigInt 0n, "" , null , undefined , NaN 
 
 //truthy
-//"0" , 'false' , " "(space) , [] , {} (object) , function(){}
+//"0" , 'false' , " "(with space) , [] , {} (object) , function(){}
 
-if(userEmail3.length === 0 ){
-    console.log("Array is empty");
+
+
+// if(userEmail3.length === 0 ){
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+if(Object.keys(emptyObj).length === 0)
+{
+    console.log("object is empty");
 }
 
+//**************** General kowldege for interview ****************** */
+//false == 0 return true
+// false == '' return true
+// 0 == '' return true
+
+
+//Nullish coalescing operator (??) : null undefined only for null & undefined
+
+let val1;
+ // val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1= undefined ?? 15
+val1= null?? 10 ??20
+console.log(val1);
+//returned value 5 , 10 , 15 , 10   instead of 10 some complex functions are cal or no answer will provide then return null
+
+
+
+//ternary operator 
+//condition ? true : false
+const iceTeaprice = 120
+iceTeaprice <= 80 ? console.log("less than 80") : console.log("more than 80");
