ef54878f16120825688dc92ea44ad464eca153aa
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-11-17 15:20:59 +0000

    thuthy,falsy values in js

---
+++ 04_control_flow/three_truthy.js
@@ -0,0 +1,19 @@
+const userEmail = "shiwani@gmail.com"
+const userEmail2 = ""
+const userEmail3 = []
+if(userEmail3){
+    console.log("got user email");
+}else{
+    console.log("do  not have user yet!");
+}
+
+//falsy values 
+//false , 0 , -0 , BigInt 0n, "" , null , undefined , NaN 
+
+//truthy
+//"0" , 'false' , " "(space) , [] , {} (object) , function(){}
+
+if(userEmail3.length === 0 ){
+    console.log("Array is empty");
+}
+
