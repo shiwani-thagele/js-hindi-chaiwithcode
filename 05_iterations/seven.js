ca9034d9cfc40d630215b8f8b0d4dc21cf13d31f
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-11-21 13:15:40 +0000

    more about map

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,8 +1,11 @@
 //map
 const mynums = [1, 2,3,4,5,6,7,8,9,10]
 
-//const newnum = mynums.map((num)=> num + 10) // OR mynums.map((num) => {return num + 10 })
+//const newnum = mynums.map((num)=> num + 10)  //automatically return value 
+// OR mynums.map((num) => {return num + 10 })
 
+
+//chaining 
 const newnum = mynums
                      .map((num) => num * 10) 
                      .map((num) => num + 1)
