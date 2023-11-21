27407f89c738a24f5c0e8e89d444fe0549f9696e
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-11-21 13:24:33 +0000

    revised map,reduceand filter

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -2,6 +2,28 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 
 
+/** 
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+0+1 =1
+1+2 =3
+3+3 =6
+6+4 =10
+**** accumulatr = accumulator+currentValue(after first value)
+
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue(0+1),
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10 
+
+*/
+
+
 const mynums = [1,2,3,4]
 // const mytotal = mynums.reduce(function(acc,currval){
 //     console.log(`accumulator ${acc} and current value ${currval}`);
