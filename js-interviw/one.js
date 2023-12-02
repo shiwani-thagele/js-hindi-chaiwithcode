713d4f1e0ffc57cf5a88e0c74203842c83c89d37
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-12-02 14:28:12 +0000

    Arrow function vs Normal function

--- js-interviw/one.js
+++ js-interviw/one.js
@@ -46,9 +46,51 @@ x =5;
 var x ;
 //console.log(x);
 
-var y =1;
+//var y =1;
 var call = function(){
-    console.log(y);
+    //console.log(y);
     var y =2;
 }
 call()
+
+// for(let i=0; i< 5; i++){ => 0,1,2,3,4
+    for(var i=0; i< 5; i++){
+    setTimeout(function(){
+       // console.log(i); //5,5,5,5,5
+    },i*1000);
+}
+
+
+//4.Array construtor from method 
+//we can convert a string => array || set => array || array to map 
+const b = Array.from('hello'); //['h','e','l','l','o']
+//console.log(b);
+let s = new Set([1,2]);
+//console.log(typeof(s));
+
+const arr0 = [1,2,3,4,5];
+const a1 = Array.from(arr0,it=>it*2); //[2,4,6,8,10]
+//console.log(a1);
+
+const a = Array.from({length:5},(it,i)=> i*2); //[0,2,4,6,8] if use 3 then we will get odd series.
+//console.log(a);
+
+//5. ARROW FUNCTION VS NORMAL FUNCTION
+
+function say(){
+    return this.name;
+}
+
+const tell = () =>{
+    return this.name;
+}
+
+const person = {
+    name:'shivi',
+    age:23,
+    tell,
+    say
+}
+
+console.log(person.say());//shivi
+console.log(person.tell());//undefined
