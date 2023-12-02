3f3bc6c40f95f10bdd824844e6ed1e88e9cc8c2c
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2023-12-02 12:24:22 +0000

    hoisting

--- js-interviw/one.js
+++ js-interviw/one.js
@@ -1,10 +1,10 @@
-//convert array to object 
+//1.convert array to object 
 
 const arr= ['a','b','c'];
 let object = arr.reduce((accumulatr,currentElement,indexOfCurrentElement) =>
     ({...accumulatr,[indexOfCurrentElement]:currentElement}),{}
 )
-console.log(object);
+//console.log(object);
 
 //OR using for loop
 
@@ -13,7 +13,7 @@ let object2 = {};
 for(let i=0; i<array.length;i++){
     object2[i]= array[i];
 }
-console.log(object2);
+//console.log(object2);
 
 //OR using forEach loop 
 
@@ -22,4 +22,33 @@ let object3 = {};
 array2.forEach((value,index)=>{
     object3[index]= value;
 });
-console.log(object3);
+//console.log(object3);
+
+
+//2. scopes tricky question
+
+(function (){
+    var x,y;
+    try{
+        throw new Error();
+    }catch(x){
+        x=1; 
+        y=2;
+        //console.log(x);//1
+    }
+    //console.log(x);//undefined
+    //console.log(y); //2
+})();
+
+//3.Hoisting
+
+x =5;
+var x ;
+//console.log(x);
+
+var y =1;
+var call = function(){
+    console.log(y);
+    var y =2;
+}
+call()
